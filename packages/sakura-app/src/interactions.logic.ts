27bc756b9402407369251d3e4a09913d157f2d75
1gy <42576495+1gy@users.noreply.github.com>
2024-05-27 12:32:25 +0000

    feat: スラッシュコマンドに応答できる機能を実装

--- packages/sakura-app/src/interactions.logic.ts
+++ packages/sakura-app/src/interactions.logic.ts
@@ -1,9 +1,11 @@
-import type {
-	Interaction,
-	InteractionResponse,
-	PongInteractionResponse,
+import {
+	InteractionType,
+	type ApplicationCommandInteraction,
+	type ChannelMessageWithSourceInteractionResponse,
+	type Interaction,
+	type InteractionResponse,
+	type PongInteractionResponse,
 } from "./discord/interactions";
-import { InteractionType } from "./discord/interactions";
 
 export const processInteraction = async (
 	interaction: Interaction,
@@ -13,5 +15,29 @@ export const processInteraction = async (
 			type: 1,
 		} satisfies PongInteractionResponse;
 	}
+	if (interaction.type === InteractionType.ApplicationCommand) {
+		return await handleApplicationCommand(interaction);
+	}
 	throw new Error("invalid interaction type");
+};
+
+export const handleApplicationCommand = async (
+	interaction: ApplicationCommandInteraction,
+): Promise<InteractionResponse> => {
+	switch (interaction.data.name) {
+		case "talk": {
+			const message = interaction.data.options?.[0]?.value ?? "";
+			const quoted = message
+				.split("\n")
+				.map((line) => `> ${line}`)
+				.join("\n");
+			return {
+				type: 4,
+				data: {
+					content: `${quoted}\nこんにちは！`,
+				},
+			} satisfies ChannelMessageWithSourceInteractionResponse;
+		}
+	}
+	throw new Error(`invalid command: ${interaction.data.name}`);
 };
