9ddd3487008a1cf530975f47bdfaf702cc28a157
1gy <42576495+1gy@users.noreply.github.com>
2025-01-29 13:30:37 +0000

    lexerの仮実装（ネスト部分未実装）

--- crates/skrc/src/lexer.rs
+++ crates/skrc/src/lexer.rs
@@ -1,2 +1,291 @@
 pub mod cursor;
 pub mod scanner;
+
+type Position = u32; // TODO: refactor to newtype
+
+pub struct Lexer<'code> {
+    code: &'code str,
+    position: Position,
+    scanner: scanner::Scanner<'code>,
+}
+
+impl Lexer<'_> {
+    fn identifier(&self, start: Position) -> super::token::TokenKind {
+        let value = &self.code[start as usize..self.position as usize];
+        super::token::TokenKind::Identifier(super::token::Identifier {
+            value: value.to_string(),
+        })
+    }
+
+    fn literal(&self, start: Position, kind: scanner::ScannedTokenKind) -> super::token::TokenKind {
+        use super::token::{Literal, LiteralKind, TokenKind};
+        use scanner::ScannedTokenKind;
+
+        let value = &self.code[start as usize..self.position as usize];
+        let literal_kind = match kind {
+            ScannedTokenKind::CharLiteral => LiteralKind::Char,
+            ScannedTokenKind::StringLiteral => LiteralKind::String,
+            ScannedTokenKind::IntegerLiteral => LiteralKind::Integer,
+            ScannedTokenKind::DecimalLiteral => LiteralKind::Decimal,
+            _ => unreachable!(),
+        };
+        TokenKind::Literal(Literal {
+            kind: literal_kind,
+            value: value.to_string(),
+        })
+    }
+
+    fn next_token(&mut self) -> super::token::Token {
+        use super::token::{Delimiter, Position, Span, Token, TokenKind};
+        use scanner::ScannedTokenKind;
+
+        loop {
+            let scanned_token = self.scanner.next_token();
+            let start = self.position;
+            self.position += scanned_token.len;
+            let token_kind = match scanned_token.kind {
+                ScannedTokenKind::Whitespace => continue, // skip
+                ScannedTokenKind::Identifier => self.identifier(start),
+                ScannedTokenKind::CharLiteral
+                | ScannedTokenKind::StringLiteral
+                | ScannedTokenKind::IntegerLiteral
+                | ScannedTokenKind::DecimalLiteral => self.literal(start, scanned_token.kind),
+                ScannedTokenKind::Equals => TokenKind::Equals,
+                ScannedTokenKind::LessThan => TokenKind::LessThan,
+                ScannedTokenKind::GreaterThan => TokenKind::GreaterThan,
+                ScannedTokenKind::Plus => TokenKind::Plus,
+                ScannedTokenKind::Minus => TokenKind::Minus,
+                ScannedTokenKind::Star => TokenKind::Star,
+                ScannedTokenKind::Slash => TokenKind::Slash,
+                ScannedTokenKind::Percent => TokenKind::Percent,
+                ScannedTokenKind::Caret => TokenKind::Caret,
+                ScannedTokenKind::And => TokenKind::And,
+                ScannedTokenKind::Or => TokenKind::Or,
+                ScannedTokenKind::Dot => TokenKind::Dot,
+                ScannedTokenKind::Comma => TokenKind::Comma,
+                ScannedTokenKind::Semicolon => TokenKind::Semicolon,
+                ScannedTokenKind::OpenParenthesis => {
+                    TokenKind::OpenDelimiter(Delimiter::Parenthesis)
+                }
+                ScannedTokenKind::CloseParenthesis => {
+                    TokenKind::CloseDelimiter(Delimiter::Parenthesis)
+                }
+                ScannedTokenKind::OpenBrace => TokenKind::OpenDelimiter(Delimiter::Brace),
+                ScannedTokenKind::CloseBrace => TokenKind::CloseDelimiter(Delimiter::Brace),
+                ScannedTokenKind::OpenBracket => TokenKind::OpenDelimiter(Delimiter::Bracket),
+                ScannedTokenKind::CloseBracket => TokenKind::CloseDelimiter(Delimiter::Bracket),
+                ScannedTokenKind::Eof => TokenKind::Eof,
+                ScannedTokenKind::Unknown => unimplemented!("Unknown token"),
+            };
+            let span =
+                Span::from_positions(Position::from_u32(start), Position::from_u32(self.position));
+            return Token::new(token_kind, span);
+        }
+    }
+
+    pub fn lex_token_trees(&mut self) -> super::token::TokenStream {
+        use super::token::{TokenKind, TokenStream, TokenTree};
+        let mut tokens = Vec::new();
+        loop {
+            let token = self.next_token();
+            match token.kind {
+                TokenKind::OpenDelimiter(_delimiter) => {
+                    todo!()
+                }
+                TokenKind::CloseDelimiter(_delimiter) => {
+                    todo!()
+                }
+                TokenKind::Eof => {
+                    return TokenStream::new(tokens);
+                }
+                _ => {
+                    tokens.push(TokenTree::Token(token));
+                }
+            }
+        }
+    }
+}
+
+pub fn lex_token_trees(code: &str) -> super::token::TokenStream {
+    let mut lexer = Lexer {
+        code,
+        position: 0,
+        scanner: scanner::Scanner::new(code),
+    };
+    lexer.lex_token_trees()
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::token::{
+        Delimiter, Identifier, Literal, LiteralKind, Position, Span, Token, TokenKind, TokenStream,
+        TokenTree,
+    };
+
+    use super::*;
+
+    #[test]
+    fn test_lexer_next_token() {
+        let code = "let x = 42;";
+        let mut lexer = Lexer {
+            code,
+            position: 0,
+            scanner: scanner::Scanner::new(code),
+        };
+        let token = lexer.next_token();
+        assert_eq!(
+            token,
+            Token::new(
+                TokenKind::Identifier(Identifier {
+                    value: "let".to_string()
+                }),
+                Span::from_positions(Position::from_u32(0), Position::from_u32(3))
+            )
+        );
+        let token = lexer.next_token();
+        assert_eq!(
+            token,
+            Token::new(
+                TokenKind::Identifier(Identifier {
+                    value: "x".to_string()
+                }),
+                Span::from_positions(Position::from_u32(4), Position::from_u32(5))
+            )
+        );
+        let token = lexer.next_token();
+        assert_eq!(
+            token,
+            Token::new(
+                TokenKind::Equals,
+                Span::from_positions(Position::from_u32(6), Position::from_u32(7))
+            )
+        );
+        let token = lexer.next_token();
+        assert_eq!(
+            token,
+            Token::new(
+                TokenKind::Literal(Literal {
+                    kind: LiteralKind::Integer,
+                    value: "42".to_string()
+                }),
+                Span::from_positions(Position::from_u32(8), Position::from_u32(10))
+            )
+        );
+        let token = lexer.next_token();
+        assert_eq!(
+            token,
+            Token::new(
+                TokenKind::Semicolon,
+                Span::from_positions(Position::from_u32(10), Position::from_u32(11))
+            )
+        );
+        let token = lexer.next_token();
+        assert_eq!(
+            token,
+            Token::new(
+                TokenKind::Eof,
+                Span::from_positions(Position::from_u32(11), Position::from_u32(11))
+            )
+        );
+    }
+
+    #[test]
+    fn test_lex_token_trees() {
+        let code = "let x = 42;";
+        let token_stream = lex_token_trees(code);
+        assert_eq!(
+            token_stream,
+            TokenStream::new(vec![
+                TokenTree::Token(Token::new(
+                    TokenKind::Identifier(Identifier {
+                        value: "let".to_string()
+                    }),
+                    Span::from_positions(Position::from_u32(0), Position::from_u32(3))
+                )),
+                TokenTree::Token(Token::new(
+                    TokenKind::Identifier(Identifier {
+                        value: "x".to_string()
+                    }),
+                    Span::from_positions(Position::from_u32(4), Position::from_u32(5))
+                )),
+                TokenTree::Token(Token::new(
+                    TokenKind::Equals,
+                    Span::from_positions(Position::from_u32(6), Position::from_u32(7))
+                )),
+                TokenTree::Token(Token::new(
+                    TokenKind::Literal(Literal {
+                        kind: LiteralKind::Integer,
+                        value: "42".to_string()
+                    }),
+                    Span::from_positions(Position::from_u32(8), Position::from_u32(10))
+                )),
+                TokenTree::Token(Token::new(
+                    TokenKind::Semicolon,
+                    Span::from_positions(Position::from_u32(10), Position::from_u32(11))
+                )),
+            ])
+        );
+    }
+
+    #[test]
+    fn test_lex_token_trees_nested() {
+        let code = "let x = (40 + 2);";
+        let token_stream = lex_token_trees(code);
+        assert_eq!(
+            token_stream,
+            TokenStream::new(vec![
+                TokenTree::Token(Token::new(
+                    TokenKind::Identifier(Identifier {
+                        value: "let".to_string()
+                    }),
+                    Span::from_positions(Position::from_u32(0), Position::from_u32(3))
+                )),
+                TokenTree::Token(Token::new(
+                    TokenKind::Identifier(Identifier {
+                        value: "x".to_string()
+                    }),
+                    Span::from_positions(Position::from_u32(4), Position::from_u32(5))
+                )),
+                TokenTree::Token(Token::new(
+                    TokenKind::Equals,
+                    Span::from_positions(Position::from_u32(6), Position::from_u32(7))
+                )),
+                TokenTree::Group(
+                    vec![
+                        TokenTree::Token(Token::new(
+                            TokenKind::OpenDelimiter(Delimiter::Parenthesis),
+                            Span::from_positions(Position::from_u32(8), Position::from_u32(9))
+                        )),
+                        TokenTree::Token(Token::new(
+                            TokenKind::Literal(Literal {
+                                kind: LiteralKind::Integer,
+                                value: "40".to_string()
+                            }),
+                            Span::from_positions(Position::from_u32(9), Position::from_u32(11))
+                        )),
+                        TokenTree::Token(Token::new(
+                            TokenKind::Plus,
+                            Span::from_positions(Position::from_u32(12), Position::from_u32(13))
+                        )),
+                        TokenTree::Token(Token::new(
+                            TokenKind::Literal(Literal {
+                                kind: LiteralKind::Integer,
+                                value: "2".to_string()
+                            }),
+                            Span::from_positions(Position::from_u32(14), Position::from_u32(15))
+                        )),
+                        TokenTree::Token(Token::new(
+                            TokenKind::CloseDelimiter(Delimiter::Parenthesis),
+                            Span::from_positions(Position::from_u32(15), Position::from_u32(16))
+                        )),
+                    ],
+                    Delimiter::Parenthesis
+                ),
+                TokenTree::Token(Token::new(
+                    TokenKind::Semicolon,
+                    Span::from_positions(Position::from_u32(16), Position::from_u32(17))
+                )),
+            ])
+        );
+    }
+}
---
+++ crates/skrc/src/token.rs
@@ -0,0 +1,113 @@
+#[derive(Debug, PartialEq)]
+pub struct Position(u32);
+
+impl Position {
+    pub fn from_u32(value: u32) -> Self {
+        Self(value)
+    }
+
+    pub fn to_u32(&self) -> u32 {
+        self.0
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Span {
+    start: u32,
+    len: u32,
+}
+
+impl Span {
+    pub fn from_positions(start: Position, end: Position) -> Self {
+        Self {
+            start: start.to_u32(),
+            len: end.to_u32() - start.to_u32(),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Identifier {
+    pub value: String,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum LiteralKind {
+    Char,
+    String,
+    Integer,
+    Decimal,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Literal {
+    pub kind: LiteralKind,
+    pub value: String,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Delimiter {
+    Parenthesis,
+    Brace,
+    Bracket,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TokenKind {
+    Identifier(Identifier),
+
+    Literal(Literal),
+
+    Equals,
+    LessThan,
+    GreaterThan,
+    Plus,
+    Minus,
+    Star,
+    Slash,
+    Percent,
+    Caret,
+    And,
+    Or,
+
+    Dot,
+    Comma,
+    Semicolon,
+
+    OpenDelimiter(Delimiter),
+    CloseDelimiter(Delimiter),
+
+    Eof,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Token {
+    pub kind: TokenKind,
+    pub span: Span,
+}
+
+impl Token {
+    pub fn new(kind: TokenKind, span: Span) -> Self {
+        Self { kind, span }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum TokenTree {
+    Token(Token),
+    Group(Vec<TokenTree>, Delimiter),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct TokenStream {
+    tokens: Vec<TokenTree>,
+}
+
+impl TokenStream {
+    pub fn new(tokens: Vec<TokenTree>) -> Self {
+        Self { tokens }
+    }
+}
+
+#[cfg(test)]
+mod tests {}
